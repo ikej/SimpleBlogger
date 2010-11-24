5600f4c1229fd464443f72fa66557ca5f06e31db
ikej <iike22@gmail.com>
2010-11-24 14:35:21 +0800

    add simple resource browse function

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,18 @@
+class ResourcesController < ApplicationController
+  def show
+    @path = path = File.join("public",params[:resource_path])
+
+    @folds = Dir[File.join(path, "*")].select{|file| File.directory?(file)}.collect do |name| 
+      fold_name = name.split('/').last 
+      fold_path = name.sub(/public\//,'')
+      {:fold_name=>fold_name,:fold_path=>'http://' + env['HTTP_HOST'] + '/' + fold_path}
+    end 
+
+    @files = Dir[File.join(path, "*")].select{|file| File.file?(file)}.collect do |name| 
+      file_name = name.split('/').last
+      file_path = name.sub(/public\//,'')
+      {:file_name => file_name,:file_path => 'http://' + env['HTTP_HOST'] + '/' + file_path, :file_type => file_name.split('.').last}
+    end
+     
+  end
+end
