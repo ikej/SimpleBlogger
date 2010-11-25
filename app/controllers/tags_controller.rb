6256b240d36de7b3756061dd8c336f1aff4fe2fb
ikej <iike22@gmail.com>
2010-11-25 16:28:25 +0800

    sort post by create date, add quick links

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
     tag = Tag.find(params[:id])
     @keyword = tag.keyword
     @tag_id = tag.id.to_s
-    @posts = tag.post.paginate :page=>params[:page],:per_page=>'10' 
+    @posts = tag.post.paginate :page=>params[:page],:per_page=>'10',:order => 'created_at DESC'  
     respond_to do |format|
       format.html
       format.atom {render 'posts/index.atom.builder', :locals=>{:posts=>tag.post}}
