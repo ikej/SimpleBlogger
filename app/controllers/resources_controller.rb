4a5de8c0309a01d8d48fc45488f07620b41d2a35
ikej <iike22@gmail.com>
2010-11-25 09:42:17 +0800

    update README, sort resources by name

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -6,13 +6,15 @@ class ResourcesController < ApplicationController
       fold_name = name.split('/').last 
       fold_path = name.sub(/public\//,'')
       {:fold_name=>fold_name,:fold_path=>'http://' + env['HTTP_HOST'] + '/' + fold_path}
-    end 
+    end
 
     @files = Dir[File.join(path, "*")].select{|file| File.file?(file)}.collect do |name| 
       file_name = name.split('/').last
       file_path = name.sub(/public\//,'')
       {:file_name => file_name,:file_path => 'http://' + env['HTTP_HOST'] + '/' + file_path, :file_type => file_name.split('.').last}
     end
-     
+
+    @folds.sort!{|fold,another_fold| fold[:fold_name] <=> another_fold[:fold_name]}
+    @files.sort!{|file,another_file| file[:file_name] <=> another_file[:file_name]}
   end
 end
