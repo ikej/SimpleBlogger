4ab763207f4dd39f50f99f57715da340031daee9
ikej <iike22@gmail.com>
2010-11-27 13:13:17 +0800

    enhance security for search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def search
     @criteria =params[:search_criteria]
     @criteria ||= flash[:last_search_criteria]
-    @search_post_results = Post.where("plain_content LIKE '%#{@criteria}%'").paginate :page=>params[:page],:per_page=>'10'
+    @search_post_results = Post.find(:all, :conditions=>["plain_content LIKE ? ", '%'+@criteria+'%']).paginate :page=>params[:page],:per_page=>'10'
     flash[:last_search_criteria] = @criteria
   end
 
@@ -10,9 +10,16 @@ class SearchController < ApplicationController
     @search_resource_results = []
     params[:search_criteria] ||= ''
     if params[:search_criteria].strip.length > 0
-      @search_resource_results = `find public/resources/ -name *#{params[:search_criteria].strip}*`.split("\n").collect do |result|
-      file_path = result.sub(/public\//,'')
-      {:name => result.split('/').last,:path=> 'http://' + env['HTTP_HOST'] + '/' + file_path,:file_type => file_path.split('.').last.upcase}
+      words_not_allowed = params[:search_criteria].scan /\||&|<|>/
+      if words_not_allowed.length > 0
+        @search_resource_results = [] 
+        
+
+      else
+        @search_resource_results = `find public/resources/ -name *#{params[:search_criteria].strip}*`.split("\n").collect do |result|
+        file_path = result.sub(/public\//,'')
+        {:name => result.split('/').last,:path=> 'http://' + env['HTTP_HOST'] + '/' + file_path,:file_type => file_path.split('.').last.upcase}
+        end
       end
     end
     render :layout=>'resource'
